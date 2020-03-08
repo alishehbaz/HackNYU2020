cd3b9d5e28eebfc1f3fb11f2f153b42d5a790619
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-03-07 22:35:48 -0500

    auth with mongodb

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+//Connect
+mongoose.connect(`mongodb://u02zm13zgdt5usfgxe6o:FNvpWyDQNbSSvN2MBi5a@bsa1w0kpy1pg7x4-mongodb.services.clever-cloud.com:27017/bsa1w0kpy1pg7x4`, { useNewUrlParser: true, useUnifiedTopology: true}).
+catch(error => console.log(error));
+
+//Schema Export
+//Nabeel was here :)
+const UsersSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const Users = mongoose.model('Users', UsersSchema, 'Users');
+
+module.exports = Users;
---
+++ server.js
@@ -0,0 +1,117 @@
+//Nabeel was here :)
+
+//Libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const md5 = require('md5')
+
+//Mongoose - Mongodb
+const User = require('./models/User')
+mongoose.connect('mongodb://u02zm13zgdt5usfgxe6o:FNvpWyDQNbSSvN2MBi5a@bsa1w0kpy1pg7x4-mongodb.services.clever-cloud.com:27017/bsa1w0kpy1pg7x4')
+
+//App Constants
+const app = express();
+
+app.use(session({
+    secret: 'du0o$o@rlKamu-#f3ecRuK',
+    resave: true,
+    saveUninitialized: true,
+    maxAge: 5000000
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'pug')
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static(__dirname + '/public')); // static public folder
+
+
+var auth = function(req, res, next) {
+    if (req.session && req.session.email) {
+      return next();
+    } else {
+      return res.redirect('/login')
+    }
+  };
+
+  var notalreadyauth = function(req, res, next) {
+    if (req.session && req.session.email) {
+        return res.redirect('/')
+    } else {
+        return next();
+    }
+  }
+
+app.get('/', auth,(req,res)=>{
+    res.send("you are logged in: "+req.session.name)
+})
+
+app.get('/login',notalreadyauth,(req,res)=>{
+    let forward = req.query.err
+    res.render('login', {message: forward})
+})
+
+app.get('/register', notalreadyauth,(req,res)=>{
+    res.render('register')
+})
+
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/login')
+});
+
+//Aarav smells :)
+app.post('/api/login', (req,res)=>{
+    let email = req.body.email
+    let password = req.body.password
+    User.find({email: email, password: md5(password)}, (err, data)=>{
+        if(err) throw err
+        console.log(data)
+        if(data[0]) {
+            req.session.name = data[0].name
+            req.session.email = email
+            res.redirect('/')
+        } else {
+            let message = "Incorrect Username or Password"
+            res.redirect('/login?err='+message)
+        }
+    })
+})
+//Nabeel was here :)
+app.post('/api/register', (req,res)=>{
+    let name = req.body.fname+" "+req.body.lname
+    let email = req.body.email
+    let password = req.body.password
+    let password1 = req.body.password1
+
+    if(password !== password1) {
+        res.redirect('/register')
+    } else {
+        var newUser = new User({
+            name: name,
+            email: email,
+            password: md5(password)
+        })
+    
+        newUser.save((err, data)=>{
+            if(err) throw err
+            console.log(data)
+            req.session.name = name
+            req.session.email = email
+            res.redirect('/')
+        })
+    }
+})
+
+//404 Router
+app.use((req, res, next) => {
+    return res.status(404);
+});
+
+//Listen
+app.listen(PORT, ()=>{
+    console.log(`Server up on port: ${PORT}`);
+});
